4efbd60d41006c10d4f17e7cfad90e3a28dc5fcc
srp10 <83276896+srp10@users.noreply.github.com>
2022-07-29 22:41:37 +0800

    commit

--- script.js
+++ script.js
@@ -104,6 +104,18 @@ let generateAxes = () => {
         .attr('transform','translate(' + padding + ', 0)')
 }
 
+const pages = document.querySelectorAll(".page");
+    const translateAmount = 100; 
+    let translate = 0;
+
+slide = (direction) => {
+        
+direction === "next" ? translate -= translateAmount : translate += translateAmount;
+    
+pages.forEach(
+        pages => (pages.style.transform = `translateX(${translate}%)`)
+     );
+}
 
 req.open('GET', url, true)
 req.onload = () => {
