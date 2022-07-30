598962fbc1cb043d9849798ebf8b5200e1f0d484
srp10 <83276896+srp10@users.noreply.github.com>
2022-07-30 19:16:52 +0800

    commit

--- script.js
+++ script.js
@@ -15,8 +15,11 @@ let padding = 40
 
 let svg = d3.select('#canvas1')
 let svgb = d3.select('#canvas2')
+let svgc = d3.select('#canvas3')
 let tooltip = d3.select('#tooltip')
 let tooltip2 = d3.select('#tooltip2')
+let tooltip3 = d3.select('#tooltip3')
+
 
 let generateScales = () => {
     
@@ -198,6 +201,98 @@ let generateAxes2 = () => {
         .attr('transform','translate(' + padding + ', 0)')
 }
 
+let generateScales3= () => {
+    
+    xScale = d3.scaleLog()
+                        .domain([d3.min(values, (item) => {
+                            return item['AverageCityMPG']
+                        }) - 1 , d3.max(values, (item) => {
+                            return item['AverageCityMPG']
+                        }) + 1])
+                        .range([padding, width-padding])
+
+    yScale = d3.scaleLog()
+                        .domain([d3.min(values, (item) => {
+                            return item['AverageHighwayMPG']
+                        }) - 1 , d3.max(values, (item) => {
+                            return item['AverageHighwayMPG']
+                        }) + 1])
+                        .range([height-padding, padding])
+
+}
+
+let drawCanvas3 = () => {
+    svgc.attr('width', width)
+    svgc.attr('height', height)
+}
+
+let drawPoints3 = () => {
+
+    svgc.selectAll('circle')
+            .data(values)
+            .enter()
+            .append('circle')
+            .attr('class', 'dot')
+            .attr('r', (item) => {
+                return (item['EngineCylinders']+6)
+            })
+            .attr('data-xvalue', (item) => {
+                return item['AverageCityMPG']
+            })
+            .attr('data-yvalue', (item) => {
+                return item['AverageHighwayMPG']
+            })
+            .attr('cx', (item) => {
+              return xScale(item['AverageCityMPG'])
+          })         
+            .attr('cy', (item) => {
+                return yScale(item['AverageHighwayMPG'])
+            })
+            .attr('fill', (item) => {
+                if(item['Fuel'] === 'Gasoline'){
+                    return 'lightgreen'
+                }else if (item['Fuel'] === 'Electricity') {
+                    return 'red'
+                }else{
+                    return 'orange'
+                }
+            })
+            .on('mouseover', (item) => {
+                tooltip3.transition()
+                    .style('visibility', 'visible')
+                
+                tooltip3.text(item['Make'] + ' - ' + item['Fuel'] + ' - ' + item['EngineCylinders'])
+            
+            })
+            .on('mouseout', (item) => {
+                tooltip3.transition()
+                    .style('visibility', 'hidden')
+            })
+}
+
+let generateAxes3 = () => {
+
+    xAxis = d3.axisBottom(xScale)
+                .tickFormat(d3.format('d'))
+                
+
+    yAxis = d3.axisLeft(yScale)
+                .tickFormat(d3.format('d'))
+
+
+    svgc.append('g')
+        .call(xAxis)
+        .attr('id', 'x-axis')
+        .attr('transform', 'translate(0, ' + (height-padding) +')')
+
+    svgc.append('g')
+        .call(yAxis)
+        .attr('id', 'y-axis')
+        .attr('transform','translate(' + padding + ', 0)')
+}
+
+
+
 const pages = document.querySelectorAll(".page");
     const translateAmount = 100; 
     let translate = 0;
@@ -219,9 +314,13 @@ req.onload = () => {
     generateScales()
     drawPoints()
     generateAxes()
-    generateScales2()
     drawCanvas2()
+    generateScales2()
     drawPoints2()
     generateAxes2()
+    drawCanvas3()
+    generateScales3()
+    drawPoints3()
+    generateAxes3()
 }
 req.send()
